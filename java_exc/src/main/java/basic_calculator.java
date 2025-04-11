88550046179ddc747a7313b6450b31e11653f7d7
WilmerBay <Wilmergarciawpm@gmail.com>
2025-04-11 13:06:24 -0700

    labeled scanner as scanboi

--- java_exc/src/main/java/basic_calculator.java
+++ java_exc/src/main/java/basic_calculator.java
@@ -2,21 +2,17 @@ import java.util.Scanner;
 
 public class basic_calculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanboi = new Scanner(System.in);
+
 
         System.out.println("Enter a number: ");
-        double num1 = scanner.nextDouble();
-        scanner.nextLine();
-
+        double num1 = scanboi.nextDouble();
+        scanboi.nextLine();
         System.out.print("Enter another number: ");
-        double num2 = scanner.nextDouble();
-        scanner.nextLine();
+        double num2 = scanboi.nextDouble();
+        scanboi.nextLine();
 
-        System.out.println("Possible calculations:");
-        System.out.println("(A)dd");
-        System.out.println("(S)ubtract");
-        System.out.println("(M)ultiply");
-        System.out.println("(D)ivide");
+
 
         double result = num1 * num2;
 
