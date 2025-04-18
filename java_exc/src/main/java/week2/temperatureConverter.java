2cfc218a29752319b5c36ae871647025632b0ea6
WilmerBay <Wilmergarciawpm@gmail.com>
2025-04-17 17:52:25 -0700

    I added the kelvin calculator, planning to loop later

--- java_exc/src/main/java/week2/temperatureConverter.java
+++ java_exc/src/main/java/week2/temperatureConverter.java
@@ -9,21 +9,34 @@ public class temperatureConverter {
 
         String tempPath = tempPath();
         double temp = getTemp();
+        System.out.println("Do you want to convert to Kelvin? Y/N");
+        String userKelvin = scanner.nextLine().toUpperCase();
+        if (userKelvin.equals("Y")) {
+            double kelvinConversion = kelvinTime(tempPath, temp);
+            System.out.println("Conversion complete Kelvin measures at: " + kelvinConversion);
+        }
         double convertedTemp = pathConversion(tempPath, temp);
+
+
         System.out.printf("Conversion complete: %.2f", convertedTemp);
         scanner.close(); // i always forget to close scanner, i think it goes here
+
     }
     static String tempPath() {
         System.out.println("Choose C for celsius or F for fahrenheit");
         String tempPath = scanner.nextLine().toUpperCase();
         return tempPath;
     }
+
+
     static double getTemp (){
         System.out.println("What's your temperature?");
         double userTemp = scanner.nextDouble();
+        scanner.nextLine();
         return userTemp;
     }
     static double pathConversion(String tempPath, double temp ) {
+
         double fahrenheit = (temp * 9/5) + 32;
         double celsius = (temp - 32) * 5/9;
         if (tempPath.equals("C")) {
@@ -37,4 +50,21 @@ public class temperatureConverter {
             return Double.NaN; // i had to look up how to return something without a number
         }
     }
+    static double kelvinTime (String tempPath, double temp) {
+
+        double celsiusToKelvin = (temp + 273.15);
+        double fahrenheitToKelvin = (temp - 32) * 5/9 + 273.15;
+        if (tempPath.equals("C")) {
+                System.out.println("Converting Celsius to Kelvin");
+                return celsiusToKelvin;
+        } else if (tempPath.equals("F")) {
+                System.out.println("Converting Fahrenheit to Kelvin");
+                return fahrenheitToKelvin;
+        } else {
+                System.out.println("We broke it, I blame you");
+                return Double.NaN;
+        }
+        }
+
+
 }
