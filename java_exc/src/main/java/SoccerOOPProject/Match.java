e3e83eebfcf04015c9ab9ff73b088704a44c9197
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-06 10:48:12 -0700

    player team and match classes made

--- java_exc/src/main/java/SoccerOOPProject/Match.java
+++ java_exc/src/main/java/SoccerOOPProject/Match.java
@@ -1,4 +1,33 @@
 package SoccerOOPProject;
 
 public class Match {
+    private Team team1;
+    private Team team2;
+    private Team winner;
+
+    public Match(Team team1, Team team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+        this.winner = null; // will be set later
+    }
+
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public Team getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Team winner) {
+        this.winner = winner;
+    }
+
+    public void playMatch() {
+        this.winner = team1;
+    }
 }
