31a3f60f09a63387c8b0e33bbc7bc488763bc0b0
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-08 11:24:00 -0700

    checking for true false variables in Room Test

--- java_exc/src/test/java/Week5/RoomTest.java
+++ java_exc/src/test/java/Week5/RoomTest.java
@@ -11,18 +11,30 @@ class RoomTest {
 
     @BeforeEach
     public void setUp() {
-        room = new Room(2, 100.0, false, false);
+        room = new Room(2, 134.0, false, false);
     }
 
     @Test
     public void testCheckInMakesRoomOccupiedAndDirty() {
+
         room.checkIn();
         assertTrue(room.isOccupied(), "Room should be occupied after check-in");
         assertTrue(room.isDirty(), "Room should be dirty after check-in");
     }
+    @Test
+    public void testCheckOutMakesRoomUnoccupiedAndClean() {
 
+        room.checkIn();
+        room.checkOut();
+        assertFalse(room.isOccupied(), "Room should not be occupied after checkout");
+        assertFalse(room.isDirty(), "Room should be clean after checkout");
+    }
 
-
-
+    @Test
+    public void testCleanRoomCleansDirtyRoom() {
+        room.setDirty(true);
+        room.cleanRoom();
+        assertFalse(room.isDirty(), "Room should be clean after cleaning");
+    }
 
 }
