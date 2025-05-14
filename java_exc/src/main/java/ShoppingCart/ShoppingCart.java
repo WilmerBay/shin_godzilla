d64ff780543f842bc72df5aca659af5b97dc67ea
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-14 15:27:35 -0700

    shopping cart logic

--- java_exc/src/main/java/ShoppingCart/ShoppingCart.java
+++ java_exc/src/main/java/ShoppingCart/ShoppingCart.java
@@ -4,17 +4,34 @@ import java.util.List;
 
 public class ShoppingCart {
 
-    private final List<Product> items;
+    private List<Product> items;
 
     public ShoppingCart() {
+
         items = new ArrayList<>();
     }
 
     public void addItem(Product product) {
+
         items.add(product);
     }
 
     public void printReceipt() {
+
         double totalBeforeTax = 0.0;
+
+        System.out.println("Receipt:");
+        for (Product item : items) {
+            double finalPrice = item.getFinalPrice();
+            System.out.printf("- %s: $%.2f%n", item.name, finalPrice);
+            totalBeforeTax += finalPrice;
+        }
+
+        double tax = totalBeforeTax * 0.21;
+        double totalAfterTax = totalBeforeTax + tax;
+
+        System.out.printf("%nSubtotal: $%.2f%n", totalBeforeTax);
+        System.out.printf("Tax (21%%): $%.2f%n", tax);
+        System.out.printf("Total: $%.2f%n", totalAfterTax);
     }
 }
