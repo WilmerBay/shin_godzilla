374dc534a2b41deca35ad19f0a3decb368c200d2
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-14 13:26:04 -0700

    fixed getters and setters

--- java_exc/src/main/java/WilmersWonderShop/Item.java
+++ java_exc/src/main/java/WilmersWonderShop/Item.java
@@ -1,6 +1,25 @@
 package WilmersWonderShop;
 
 public abstract class Item {
+
+    public abstract void use();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     protected String name;
     protected double price;
 
@@ -9,13 +28,7 @@ public abstract class Item {
         this.price = price;
     }
 
-    public abstract void use();
 
-    public String getName() {
-        return name;
-    }
 
-    public double getPrice() {
-        return price;
-    }
+
 }
