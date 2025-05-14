07a3b36b348abc05bbd303df83f555663b47791a
WilmerBay <Wilmergarciawpm@gmail.com>
2025-05-14 14:50:00 -0700

    shopping cart logic added

--- java_exc/src/main/java/ShoppingCart/DiscountedProduct.java
+++ java_exc/src/main/java/ShoppingCart/DiscountedProduct.java
@@ -1,4 +1,18 @@
 package ShoppingCart;
 
-public class DiscountedProduct {
+public class DiscountedProduct extends Product {
+
+    private final double discountPercentage;
+
+    public DiscountedProduct(String name, double price, double discountPercentage) {
+
+        super(name, price);
+        this.discountPercentage = discountPercentage;
+    }
+
+    @Override
+    public double getFinalPrice() {
+
+        return price - (price * discountPercentage / 100);
+    }
 }
--- java_exc/src/main/java/ShoppingCart/ShoppingCart.java
+++ java_exc/src/main/java/ShoppingCart/ShoppingCart.java
@@ -1,4 +1,20 @@
 package ShoppingCart;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingCart {
+
+    private final List<Product> items;
+
+    public ShoppingCart() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(Product product) {
+        items.add(product);
+    }
+
+    public void printReceipt() {
+        double totalBeforeTax = 0.0;
+    }
 }
